eb285de865894df915c68b8c78f4a6471e1c7a06
Maximilian-Kay <dkarampinas96@gmail.com>
2022-10-20 23:23:54 +0300

    added exit button to calendar form ,deleted unneeded calendar form

--- calendarForm.Designer.cs
+++ calendarForm.Designer.cs
@@ -40,13 +40,15 @@
             this.label7 = new System.Windows.Forms.Label();
             this.daycontainer = new System.Windows.Forms.FlowLayoutPanel();
             this.lbdate = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnprev
             // 
-            this.btnprev.Location = new System.Drawing.Point(951, 680);
+            this.btnprev.Location = new System.Drawing.Point(713, 552);
+            this.btnprev.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnprev.Name = "btnprev";
-            this.btnprev.Size = new System.Drawing.Size(109, 34);
+            this.btnprev.Size = new System.Drawing.Size(82, 28);
             this.btnprev.TabIndex = 1;
             this.btnprev.Text = "Προηγούμενο";
             this.btnprev.UseVisualStyleBackColor = true;
@@ -54,9 +56,10 @@
             // 
             // btnnext
             // 
-            this.btnnext.Location = new System.Drawing.Point(1070, 680);
+            this.btnnext.Location = new System.Drawing.Point(802, 552);
+            this.btnnext.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnnext.Name = "btnnext";
-            this.btnnext.Size = new System.Drawing.Size(100, 34);
+            this.btnnext.Size = new System.Drawing.Size(75, 28);
             this.btnnext.TabIndex = 2;
             this.btnnext.Text = "Επόμενο";
             this.btnnext.UseVisualStyleBackColor = true;
@@ -65,40 +68,44 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(46, 60);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 26);
+            this.label1.Size = new System.Drawing.Size(84, 25);
             this.label1.TabIndex = 3;
             this.label1.Text = "Κυριακή";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(212, 60);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(159, 49);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(94, 26);
+            this.label2.Size = new System.Drawing.Size(93, 25);
             this.label2.TabIndex = 4;
             this.label2.Text = "Δευτέρα";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(387, 60);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(290, 49);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(66, 26);
+            this.label3.Size = new System.Drawing.Size(64, 25);
             this.label3.TabIndex = 5;
             this.label3.Text = "Τρίτη";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(539, 60);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(404, 49);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(92, 26);
+            this.label4.Size = new System.Drawing.Size(93, 25);
             this.label4.TabIndex = 4;
             this.label4.Text = "Τετάρτη";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -106,55 +113,71 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(706, 60);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(530, 49);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(86, 26);
+            this.label5.Size = new System.Drawing.Size(82, 25);
             this.label5.TabIndex = 4;
             this.label5.Text = "Πέμπτη";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(849, 60);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(637, 49);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(125, 26);
+            this.label6.Size = new System.Drawing.Size(113, 25);
             this.label6.TabIndex = 4;
             this.label6.Text = "Παρασκευή";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(1032, 60);
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(774, 49);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(95, 26);
+            this.label7.Size = new System.Drawing.Size(91, 25);
             this.label7.TabIndex = 6;
             this.label7.Text = "Σάββατο";
             // 
             // daycontainer
             // 
-            this.daycontainer.Location = new System.Drawing.Point(19, 89);
+            this.daycontainer.Location = new System.Drawing.Point(14, 72);
+            this.daycontainer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.daycontainer.Name = "daycontainer";
-            this.daycontainer.Size = new System.Drawing.Size(1151, 585);
+            this.daycontainer.Size = new System.Drawing.Size(863, 475);
             this.daycontainer.TabIndex = 0;
             // 
             // lbdate
             // 
-            this.lbdate.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbdate.Location = new System.Drawing.Point(353, 9);
+            this.lbdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbdate.Location = new System.Drawing.Point(265, 7);
+            this.lbdate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbdate.Name = "lbdate";
-            this.lbdate.Size = new System.Drawing.Size(468, 38);
+            this.lbdate.Size = new System.Drawing.Size(351, 31);
             this.lbdate.TabIndex = 7;
             this.lbdate.Text = "Τετάρτη";
             this.lbdate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 552);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 28);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Έξοδος";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // calendarForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1191, 741);
+            this.ClientSize = new System.Drawing.Size(893, 602);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lbdate);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -168,6 +191,7 @@
             this.Controls.Add(this.daycontainer);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "calendarForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "x";
@@ -189,5 +213,6 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.FlowLayoutPanel daycontainer;
         private System.Windows.Forms.Label lbdate;
+        private System.Windows.Forms.Button button1;
     }
 }
